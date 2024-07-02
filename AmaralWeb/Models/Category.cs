3626c7b8c47085c364332b913e06157ba8f2b73d
Amaral-tec <amaral_adm@hotmail.com>
2024-07-02 10:19:33 -0300

    Adding Category model class

---
+++ AmaralWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmaralWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+    }
+}
--- AmaralWeb/Program.cs
+++ AmaralWeb/Program.cs
@@ -1,7 +1,12 @@
+using AmaralWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
