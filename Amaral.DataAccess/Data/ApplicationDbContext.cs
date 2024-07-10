c969f881f3912a67e0b2982018df493ebd143dd9
Amaral-tec <amaral_adm@hotmail.com>
2024-07-10 17:16:52 -0300

    add Scaffold Identity Issue

--- Amaral.DataAccess/Data/ApplicationDbContext.cs
+++ Amaral.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Amaral.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Amaral.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -15,6 +16,8 @@ namespace Amaral.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                new Category { Id = 1, Name = "Action", Description = "Books full of excitement and adventure." },
                new Category { Id = 2, Name = "SciFi", Description = "Science fiction books exploring futuristic concepts." },
--- AmaralWeb/Areas/Customer/Controllers/HomeController.cs
+++ AmaralWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Amaral.DataAccess.Repository.IRepository;
 using Amaral.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,26 @@ namespace AmaralWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Product> productList = [.. _unitOfWork.Product.GetAll(includeProperties: "Category")];
+
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+
+            return View(product);
         }
 
         public IActionResult Privacy()
