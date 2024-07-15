086d0b650a176c31dab35a06d336c1256c776f6d
Amaral-tec <amaral_adm@hotmail.com>
2024-07-15 01:25:25 -0300

    add login by Facebook API

--- Amaral.Models/ApplicationUser.cs
+++ Amaral.Models/ApplicationUser.cs
@@ -5,13 +5,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Amaral.Models
 {
-    public class ApplicationUser:IdentityUser
+    public class ApplicationUser : IdentityUser
     {
         [Required]
         public string Name { get; set; }
+
+        [Display(Name = "Street Address")]
         public string? StreetAddress { get; set; }
         public string? City { get; set;}
         public string? State { get; set;}
+
+        [Display(Name = "Postal Code")]
         public string? PostalCode { get; set;}
         public int? CompanyId { get; set;}
 
--- AmaralWeb/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ AmaralWeb/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -17,6 +17,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using Amaral.Models;
+using Amaral.Utility;
 
 namespace AmaralWeb.Areas.Identity.Pages.Account
 {
@@ -84,8 +86,16 @@ namespace AmaralWeb.Areas.Identity.Pages.Account
             [Required]
             [EmailAddress]
             public string Email { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
-        
+
         public IActionResult OnGet() => RedirectToPage("./Login");
 
         public IActionResult OnPost(string provider, string returnUrl = null)
@@ -131,7 +141,8 @@ namespace AmaralWeb.Areas.Identity.Pages.Account
                 {
                     Input = new InputModel
                     {
-                        Email = info.Principal.FindFirstValue(ClaimTypes.Email)
+                        Email = info.Principal.FindFirstValue(ClaimTypes.Email),
+                        Name = info.Principal.FindFirstValue(ClaimTypes.Name)
                     };
                 }
                 return Page();
@@ -155,10 +166,18 @@ namespace AmaralWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Name = Input.Name;
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
 
                 var result = await _userManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, StaticData.ROLE_CUSTOMER);
+                    
                     result = await _userManager.AddLoginAsync(user, info);
                     if (result.Succeeded)
                     {
@@ -197,11 +216,11 @@ namespace AmaralWeb.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
